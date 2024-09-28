69840d991e34d3b07b8811ca5634a5bfedd8a4f9
Deekshigowda08 <149522963+Deekshigowda08@users.noreply.github.com>
2024-09-28 14:36:27 +0530

    3rd

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ const CryptoJS = require("crypto-js");
 export default function Example() {
   const [email, setemail] = useState()
   const [password, setpassword] = useState()
+  const [selected, setSelected] = useState("User");
 
     const [isClient, setIsClient] = useState(false);
   
@@ -40,10 +41,15 @@ export default function Example() {
             body: JSON.stringify({email:decoded.email, password:originalText})
           })
           const result= await data.json()
-      
+          const select = localStorage.getItem('select');
           if (result.success) {
+            if (select === "User"){
+              window.location.replace(`/user_page?token=${token}`)
+            }
+            else{
+              window.location.replace(`/acceptor_page?token=${token}`)
+            }
             
-            window.location.replace(`/main_page?token=${token}`)
           } else {
             console.error('Login failed:', result.message);
            
@@ -76,12 +82,20 @@ export default function Example() {
     if(result.success)
      {
       localStorage.setItem('Token',result.token)
+      localStorage.setItem('select',selected)
       const tok = localStorage.getItem('Token');
+
       toast("Success")
       setemail("")
       setpassword("")
       setTimeout(() => {
-        window.location.replace(`/main_page?token=${tok}`)
+        if (selected === "User"){
+          window.location.replace(`/user_page?token=${tok}`)
+        }
+        else{
+          window.location.replace(`/acceptor_page?token=${tok}`)
+        }
+        
       }, 500);}
       else{
         
@@ -98,16 +112,34 @@ return (
   <div className="flex min-h-screen bg-[#f7f7f7] flex-1 flex-col justify-center px-6 py-12 lg:px-[20%]"> 
     <div className="flex min-h-full border bg-white shadow-lg shadow-slate-200 md:shadow-xl md:shadow-slate-200 flex-1 flex-col justify-center px-6  py-12 lap:py-2 lg:px-8">
     <ToastContainer />
-      <div className="lap:mx-auto lap:w-full bg-white lap:max-w-sm">
-        <img
-          alt="Expenditure manager"
-          src="https://cdn1.iconfinder.com/data/icons/shopping-essentials-3/32/invoice-bill-statement-purchase-expenditure-cost-shopping-1024.png"
-          className="mx-auto bg-white h-10 w-auto"
-        />
-        <h2 className="mt-10 text-center bg-white text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to your account
-        </h2>
-      </div>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <div>
+        <button
+          onClick={() => setSelected("User")}
+          style={{
+            padding: "10px 20px",
+            margin: "0px",
+            backgroundColor: selected === "User" ? "#4f46e5" : "white",
+            border: "1px solid black",
+            cursor: "pointer",
+          }}
+        >
+          User
+        </button>
+
+        <button
+          onClick={() => setSelected("Acceptor")}
+          style={{
+            padding: "10px 20px",
+            margin: "0px",
+            backgroundColor: selected === "Acceptor" ? "#4f46e5" : "white",
+            border: "1px solid black",
+            cursor: "pointer",
+          }}
+        >
+          Acceptor
+        </button>
+      </div></div>
 
       <div className="mt-10 lap:mx-auto bg-white lap:w-full lap:max-w-sm">
         <form onSubmit={Submit} method="POST" className="bg-white space-y-6">
