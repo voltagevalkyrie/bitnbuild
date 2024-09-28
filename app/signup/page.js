639cfd2cf708b4dbd9a830c3c76557e19abac297
Gagan-poojari <159212673+Gagan-poojari@users.noreply.github.com>
2024-09-28 22:04:30 +0530

    commit 2

--- app/signup/page.js
+++ app/signup/page.js
@@ -7,19 +7,19 @@ export default function Example() {
   const [name, setname] = useState()
   const [email, setemail] = useState()
   const [password, setpassword] = useState()
-  
-  const Submit=async(e)=>{
+
+  const Submit = async (e) => {
     e.preventDefault()
-    console.log(name,password,email);
-    const data= await fetch("/api", {
+    console.log(name, password, email);
+    const data = await fetch("/api", {
       method: "post",
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({name,password,email})
+      body: JSON.stringify({ name, password, email })
     })
-    if(data.ok){
+    if (data.ok) {
       toast("Done!")
       setemail("")
       setname("")
@@ -27,96 +27,93 @@ export default function Example() {
       setTimeout(() => {
         window.location.replace("/")
       }, 2000);
-      
+
 
     }
 
   }
-    return (
-      <div className="flex min-h-screen bg-[#f7f7f7] flex-1 flex-col  justify-center px-6 py-12 lg:px-[20%]">
-        <div className="flex h-full border bg-white shadow-lg shadow-slate-200 md:shadow-xl md:shadow-slate-200 flex-1 flex-col justify-center px-6  py-12 lap:py-0 lap:pb-6 lg:px-8">
-        <ToastContainer />
-          <div className="lap:mx-auto lap:w-full bg-white lap:max-w-sm">
-            <img
-              alt="Expenditure manager"
-              src="https://cdn1.iconfinder.com/data/icons/shopping-essentials-3/32/invoice-bill-statement-purchase-expenditure-cost-shopping-1024.png"
-              className="mx-auto h-10 bg-white w-auto"
-            />
-            <h2 className="mt-10 text-center text-2xl bg-white font-bold leading-9 tracking-tight text-gray-900">
-             Just Sign up
-            </h2>
-          </div>
-  
-          <div className="mt-10 lap:mx-auto bg-white lap:w-full lap:max-w-sm">
-            <form  onSubmit={Submit} method="POST" className="bg-white space-y-6">
-            <div className="bg-white">
-                <label htmlFor="name" className="block text-sm font-medium bg-white leading-6 text-gray-900">
-                  Name
+  return (
+    <div className="flex bg-[#f4f4f4] h-[100vh] flex-col justify-center w-[100vw]">
+      <ToastContainer />
+      <div className="text-center">
+        <div className=" font-bold text-3xl">
+           Organize yourself... Help others !!!
+        </div>
+        <h2 className="mt-5 text-center text-2xl  font-semibold leading-9 tracking-tight text-gray-900">
+          Get Started
+        </h2>
+      </div>
+
+      <div className="mt-5 w-[30%] h-[70vh] lap:mx-auto bg-[#00000007] shadow1 rounded-xl flex items-center justify-center">
+        <div className=" lap:w-full lap:max-w-sm h-full">
+          <form onSubmit={Submit} method="POST" className="space-y-6 flex flex-col gap-2 pt-10">
+            <div className="">
+              <label htmlFor="name" className="text-[15px] font-medium text-[#303030]">
+                Name
+              </label>
+              <div className="mt-2 ">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  value={name}
+                  onChange={(e) => { setname(e.target.value) }}
+                  required
+                  className="block w-full rounded-md border-0 p-3 text-gray-900 font-medium shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset outline-none focus:ring-[#0000002b] lap:text-sm lap:leading-6"
+                />
+              </div>
+            </div>
+            <div className="">
+              <label htmlFor="email" className="text-[15px] font-medium text-[#303030]">
+                Email address
+              </label>
+              <div className="mt-2 ">
+                <input
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => { setemail(e.target.value) }}
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 p-3 text-gray-900 font-medium shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset outline-none focus:ring-[#0000002b] lap:text-sm lap:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="">
+              <div className="text-[15px] font-medium text-[#303030]">
+                <label htmlFor="password" className="block  text-sm font-medium leading-6 text-gray-900">
+                  Password
                 </label>
-                <div className="mt-2 bg-white">
-                  <input
-                    id="name"
-                    name="name"
-                    type="text"
-                    value={name}
-                    onChange={(e)=>{setname(e.target.value)}}
-                    required
-                    className="block px-4 w-full bg-white rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 lap:text-sm lap:leading-6"
-                  />
-                </div>
+
               </div>
-              <div className="bg-white">
-                <label htmlFor="email" className="block text-sm font-medium leading-6 bg-white text-gray-900">
-                  Email address
-                </label>
-                <div className="mt-2 bg-white">
-                  <input
-                    id="email"
-                    name="email"
-                    value={email}
-                    onChange={(e)=>{setemail(e.target.value)}}
-                    type="email"
-                    required
-                    autoComplete="email"
-                    className="block bg-white px-4 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 lap:text-sm lap:leading-6"
-                  />
-                </div>
+              <div className="mt-2 ">
+                <input
+                  id="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => { setpassword(e.target.value) }}
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md border-0 p-3 text-gray-900 font-medium shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset outline-none focus:ring-[#0000002b] lap:text-sm lap:leading-6"
+                />
               </div>
-  
-              <div className="bg-white">
-                <div className="flex bg-white items-center justify-between">
-                  <label htmlFor="password" className="block bg-white text-sm font-medium leading-6 text-gray-900">
-                    Password
-                  </label>
-                  
-                </div>
-                <div className="mt-2 bg-white">
-                  <input
-                    id="password"
-                    name="password"
-                    value={password}
-                    onChange={(e)=>{setpassword(e.target.value)}}
-                    type="password"
-                    required
-                    autoComplete="current-password"
-                    className="block px-4 w-full bg-white rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 lap:text-sm lap:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign up
-                </button>
-              </div>
-            </form>
-  
-            
-          </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="smooth flex w-full justify-center rounded-md bg-indigo-600 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#000] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
+
         </div>
       </div>
-    )
-  }  
+    </div>
+  )
+}  
